50b181a623fdab467170effa1f90ffb64311cc9c
daniel-barbu <63318773+daniel-barbu@users.noreply.github.com>
2020-04-17 20:39:56 +0300

    Update code.js

--- code.js
+++ code.js
@@ -5,23 +5,22 @@ if (!(window.location.href.includes("miniclip.com/games/8-ball-pool-multiplayer/
   }
 }
 
-//zoom & remove unnecessary
+//remove unnecessary, zoom & center
 document.getElementsByClassName("sky-wrapper")[0].remove();
 document.getElementById("site-header").remove();
 document.getElementById("pool-menu-after").remove();
 document.getElementsByClassName("tools")[0].remove();
 document.getElementsByClassName("tools")[1].remove();
 
-document.getElementById("game-container").style="transform-origin:0px 0px; transform:scale("+Math.min(
-  window.innerWidth/document.getElementById("iframe-game").contentWindow.document.getElementById("container").style.width.replace("px",""), 
-  window.innerWidth/document.getElementById("iframe-game").contentWindow.document.getElementById("container").style.height.replace("px",""))+");";
-
 document.getElementById("iframe-game").contentWindow.document.getElementById("container").style.margin="0px";
-document.getElementById("game-embed").style.height=screen.height-20+"px"
-document.getElementById("game-embed").style.width=screen.width-20+"px"
-document.getElementById("game-container").style="margin:0;"
+document.getElementById("game-embed").style.height=window.innerHeight+"px";
+document.getElementById("game-embed").style.width=window.innerWidth+"px";
 document.getElementsByClassName("expert-game")[0].style="padding-right:0;"
 document.body.style.overflow="hidden";
+document.getElementById("game-container").style="margin:0 !important; transform-origin:0px 0px; transform:scale("+Math.min(window.innerWidth/document.getElementById("iframe-game").contentWindow.document.getElementById("container").style.width.replace("px",""), (window.outerHeight-100)/document.getElementById("iframe-game").contentWindow.document.getElementById("container").style.height.replace("px",""))+");";
+
+if ((window.outerHeight-100)/document.getElementById("iframe-game").contentWindow.document.getElementById("container").style.height.replace("px","")<window.innerWidth/document.getElementById("iframe-game").contentWindow.document.getElementById("container").style.width.replace("px",""))
+	document.getElementById("iframe-game").contentWindow.document.getElementById("container").style.margin="auto";
 
 //LINES(6)
 var img = [document.createElement("img"),document.createElement("img"),document.createElement("img"),document.createElement("img"),document.createElement("img"),document.createElement("img")];
